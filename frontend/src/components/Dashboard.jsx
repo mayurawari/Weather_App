52fb26234b7ff91beabeea4ad525872d70103603
mayurawari <117013117+mayurawari@users.noreply.github.com>
2024-08-09 22:25:08 +0530

    backend added

---
+++ frontend/src/components/Dashboard.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import "../styles/DashB.css"
+// A main component to display the weather dashboard.
+// A search component to enter the city name.
+// A weather display component for current weather and forecast.
+// A favorite component to display and manage favorite cities.
+
+export const Dashboard = () => {
+  return (
+    <div className='mainbox'>
+
+    </div>
+  )
+}
+// https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+//4cf3bacbbc96663b0be8529f51b31905
